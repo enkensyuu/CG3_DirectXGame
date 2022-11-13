287bce2f7c91ca3b1e4943147b6a1db5f3252223
DESKTOP-8KS70PI\siyum <k021g1321@g.neec.ac.jp>
2022-11-13 20:46:48 +0900

    02_03

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,31 +1,20 @@
 #include "BasicShaderHeader.hlsli"
 // 三角形の入力から、三角形を2つ出力
-[maxvertexcount(6)]
+[maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION,
+	point VSOutput input[1] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 )
 {
-	//	1つ目の三角形
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;					//	出力用頂点データ
-		element.svpos = input[i].svpos;		//	頂点座標をコピー
-		element.normal = input[i].normal;	//	法線をコピー
-		element.uv = input[i].uv;			//	UVをコピー
-		//	頂点を1つ出力(出力リストに追加)
+		GSOutput element;				
+		element.normal = input[0].normal;
+		element.uv = input[0].uv;
+		element.svpos = input[0].svpos;
 		output.Append(element);
-	}
-	//	現在のストリップを終了
-	output.RestartStrip();
-	// 2つ目の三角形
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos + float4(20.0f, 0.0f, 0.0f, 0.0f);
-		element.normal = input[i].normal;
-		element.uv = input[i].uv*5.0f;
-		//	頂点を1つ出力(出力リストに追加)
+
+		element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0.0f, 0.0f);
 		output.Append(element);
-	}
+
+		element.svpos = input[0].svpos + float4(10.0f, 0.0f, 0.0f, 0.0f);
+		output.Append(element);
 }
