00e7179002f7426c13289529f44e0f9d7ef6335a
DESKTOP-8KS70PI\siyum <k021g1321@g.neec.ac.jp>
2022-11-13 21:52:12 +0900

    02_04

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,20 +1,36 @@
 #include "BasicShaderHeader.hlsli"
-// 三角形の入力から、三角形を2つ出力
-[maxvertexcount(3)]
+
+static const uint vnum = 4;
+
+static const float4 offset_array[vnum] =
+{
+	float4(-0.5f,-0.5f,0,0),
+	float4(-0.5f,+0.5f,0,0),
+	float4(+0.5f,-0.5f,0,0),
+	float4(+0.5f,+0.5f,0,0)
+};
+
+static const float2 uv_array[vnum] =
+{
+	float2(0,1),
+	float2(0,0),
+	float2(1,1),
+	float2(1,0)
+};
+
+
+[maxvertexcount(vnum)]
 void main(
 	point VSOutput input[1] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 )
 {
-		GSOutput element;				
-		element.normal = input[0].normal;
-		element.uv = input[0].uv;
-		element.svpos = input[0].svpos;
+	GSOutput element;
+	for (uint i = 0; i < vnum; i++)
+	{
+		element.svpos = input[0].pos + offset_array[i];
+		element.svpos = mul(mat, element.svpos);
+		element.uv = uv_array[i];
 		output.Append(element);
-
-		element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0.0f, 0.0f);
-		output.Append(element);
-
-		element.svpos = input[0].svpos + float4(10.0f, 0.0f, 0.0f, 0.0f);
-		output.Append(element);
+	}
 }
